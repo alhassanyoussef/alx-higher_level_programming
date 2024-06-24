6dc7f927c1e7fab6a53f3631ad7e8d5e15ef9eab
alhassanyoussef <hyoussef650@.com>
2024-06-24 18:51:42 +0300

    mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,32 +5,32 @@ Module that connects a python script to a database
 
 if __name__ == "__main__":
 
-        import MySQLdb
-            from sys import argv
+    import MySQLdb
+    from sys import argv
 
-                # Connect database using command-line arguments
-                    my_database = MySQLdb.connect(
-                                    host='localhost',
-                                            user=argv[1],
-                                                    password=argv[2],
-                                                            db=argv[3],
-                                                                    port=3306
-                                                                        )
-                        # define cursor used to execute mysql queries
-                            my_cursor = my_database.cursor()
+    # Connect database using command-line arguments
+    my_database = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
+    )
+    # define cursor used to execute mysql queries
+    my_cursor = my_database.cursor()
 
-                                # Execute a SELECT query to select data
-                                    my_cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    # Execute a SELECT query to select data
+    my_cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
 
-                                        # fetch all the data returned by the query
-                                            my_data = my_cursor.fetchall()
+    # fetch all the data returned by the query
+    my_data = my_cursor.fetchall()
 
-                                                # Iterate through the fetched data and print each row
-                                                    for row in my_data:
-                                                                print(row)
+    # Iterate through the fetched data and print each row
+    for row in my_data:
+        print(row)
 
-                                                                    # Close all cursors
-                                                                        my_cursor.close()
+    # Close all cursors
+    my_cursor.close()
 
-                                                                            # Close all databases
-                                                                                my_database.close()
+    # Close all databases
+    my_database.close()
